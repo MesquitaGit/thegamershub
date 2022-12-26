c83fff9c279d67e716140dc339884787091c46a9
Marta <martinha_peres@hotmail.com>
2022-12-26 18:56:41 +0000

    added api call for homepage game cards

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
-// ********* require fileUploader in order to use it *********
-const fileUploader = require("../config/cloudinary.config");
+const ApiService = require("../services/api.service");
+const apiService = new ApiService();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", async (req, res, next) => {
+  try {
+    const allGames = await apiService.getAllGames();
+    const gameCardItems = allGames.data.results.slice(0, 4);
+
+    res.render("index", {
+      gameCardItems,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
